1727d667d7bb15b5805a8d6b617be39fd26e30a5
Raschell Jarquin <raschelljq0203@estudiantec.cr>
2023-11-20 14:34:05 -0600

    Perfil de Usuario
    Perfil de usuario

--- Producto/TEC/Front-End/src/components/PerfilUsuario.jsx
+++ Producto/TEC/Front-End/src/components/PerfilUsuario.jsx
@@ -1,31 +1,48 @@
 import React from 'react';
 import Menu from './Menu';
 import Footer from './Footer'
-import Usuario from '../images/Fabricio.jpg'
+import Usuario from '../images/usuario.png'
+
 
 const PerfilUsuario = () =>{
     return(
-        <div>
+        <div className='perfilUsuario'>
             <header>
                 <Menu/>
             </header>
-            <div className='profile-container'>
-            <div className="profile">
-            <div className="profile-picture">
-            <img src={Usuario}/>
+            <div className='contenedor'>
+            <div class="left box-primary">
+            <img className = 'image' src={Usuario}/>
+            <h3 class="username text-center">Usuario 1</h3>
+            <a href="#" class="btn btn-primary btn-block"><b>Guardar cambios</b></a>
             </div>
-            <div className="profile-details">
-            <h2>Nombre: Fabricio</h2>
-            <h2>Apellidos: Porras Morera</h2>
-            <p>Correo: fabri@example.com</p>
-            <p>Teléfono: 123-456-7890</p>
-            <p>Contraseña: ********</p>
+            <div className='right tab-content'>
+                <form className='form-horizontal'>
+                    <div className='form-group'>
+                    <label for="inputName" class="col-sm-2 control-label">Nombre</label>
+                    <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputName" placeholder="Nome"/>
+                    </div>
+                    <label for="inputName" class="col-sm-2 control-label">Primer Apellido</label>
+                    <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputName" placeholder="Nome"/>
+                    </div>
+                    <label for="inputName" class="col-sm-2 control-label">Segundo Apellido</label>
+                    <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputName" placeholder="Nome"/>
+                    </div>
+                    <label for="inputName" class="col-sm-2 control-label">Correo</label>
+                    <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputName" placeholder="Nome"/>
+                    </div>
+                    <label for="inputName" class="col-sm-2 control-label">Contraseña</label>
+                    <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputName" placeholder="Nome"/>
+                    </div>
+                    </div>
+                </form>
             </div>
             </div>
-            </div>
-            <Footer>
-                <Footer/>
-            </Footer>
         </div>
     );
 }
